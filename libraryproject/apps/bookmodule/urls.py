f0f04f855f12c99177b80a7b1a80b5f37fc516ad
Lama <lamashayea0@gmail.com>
2026-04-01 10:12:13 +0300

    Add files via upload

---
+++ libraryproject/apps/bookmodule/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('html5/links', views.links_view, name='links'),
+    path('html5/text/formatting', views.formatting_view, name='formatting'),
+    path('html5/listing', views.listing_view, name='listing'),
+    path('html5/tables', views.tables_view, name='tables'),
+]
+
---
+++ libraryproject/apps/bookmodule/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+
+def links_view(request):
+    return render(request, 'bookmodule/links.html')
+
+def formatting_view(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def listing_view(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables_view(request):
+    return render(request, 'bookmodule/tables.html')
